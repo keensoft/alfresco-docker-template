b0b7aaca2e3f09b00dc634b106a0d8ad0d830af0
Mikel Asla <mikel.asla@keensoft.es>
2018-03-16 17:02:36 +0100

    some dockerfile cleaning

--- libreoffice/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --name="oooserver" -d xcgd/libreoffice-server
--- libreoffice/startoo.sh
+++ libreoffice/startoo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/libreoffice --nologo --norestore --invisible --headless --accept='socket,host=0,port=8997,tcpNoDelay=1;urp;'
+/usr/bin/libreoffice --nologo --norestore --invisible --headless --accept='socket,host=0,port=8100,tcpNoDelay=1;urp;'
